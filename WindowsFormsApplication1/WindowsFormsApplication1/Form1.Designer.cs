416e73a85233878030a633ff5fe41b3dc241789a
MJ <70Raouf@gmail.com>
2019-07-10 20:03:18 +0430

    commit

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(183, 102);
+            this.button2.Location = new System.Drawing.Point(183, 72);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(86, 57);
             this.button2.TabIndex = 0;
